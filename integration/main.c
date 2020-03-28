7bd45a8c380febd815345da953e267a338a82e9b
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-03-28 14:25:24 +0100

    Zebranie sum cząstkowych

--- integration/main.c
+++ integration/main.c
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
 
     // printf("I'm process %d of %d and I'll integrate soon\n", world_rank, world_size);
 
-    double result = integrate(x2, 0, 2, 10);
+    double result = integrate(x2, 0, 2, 100000);
     printf("Result: %lf\n", result);
 
     MPI_Finalize();
@@ -103,7 +103,6 @@ double integrate(double (*func)(double), double begin, double end, int num_point
         // }
 
         Pair *ranges = splitIntoRanges(num_points, world_size);
-        Pair myRange = ranges[0];
         // printf("My range: [%d, %d)\n", myRange.a, myRange.b);
 
         // Send data to other processes
@@ -121,26 +120,42 @@ double integrate(double (*func)(double), double begin, double end, int num_point
         // double chetedResult = integrateRange(func, nodes, num_points);
         double chetedResult = -1;
 
+        // Calculate my part
+        partialSums[0] = integrateRange(func, nodes, ranges[0].b - ranges[0].a);
+
+        // Receive results
+        for (int i = 1; i < world_size; i++)
+        {
+            MPI_Recv(&partialSums[i], 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        double sum = 0.;
+        for (int i = 0; i < world_size; i++)
+        {
+            sum += partialSums[i];
+        }
+
         free(ranges);
         free(nodes);
-        return chetedResult;
+        return sum;
     }
     else
     {
         int nodesNumber;
         MPI_Recv(&nodesNumber, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Process %d -> nodesNumber = %d\n", world_rank, nodesNumber);
+        // printf("Process %d -> nodesNumber = %d\n", world_rank, nodesNumber);
 
         double nodes[nodesNumber];
         MPI_Recv(nodes, nodesNumber, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for (int i = 0; i < nodesNumber; i++)
-        {
-            printf("Process %d -> node[%d] = %lf\n", world_rank, i, nodes[i]);
-        }
+        // for (int i = 0; i < nodesNumber; i++)
+        // {
+        //     printf("Process %d -> node[%d] = %lf\n", world_rank, i, nodes[i]);
+        // }
 
         double partialResult = integrateRange(func, nodes, nodesNumber);
         // printf("Process %d -> partial sum = %lf\n", world_rank, partialResult);
-        // printf("Process %d will work on range [%d, %d)\n", world_rank, rangeStart, rangeEnd);
+
+        MPI_Send(&partialResult, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
 
     return -1;
