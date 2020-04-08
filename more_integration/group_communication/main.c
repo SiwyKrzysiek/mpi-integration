1d5a01ee536d083765e97de95f7b4c1aa71547b4
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-08 13:35:08 +0200

    Remove unnecessary imports

--- more_integration/group_communication/main.c
+++ more_integration/group_communication/main.c
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "pair.h"
-#include "utils.h"
 
 // y = x^2
 double x2(double x)
--- more_integration/group_communication/pair.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Simple struct for storing pari of numbers
-typedef struct Pair
-{
-    int a, b;
-} Pair;
